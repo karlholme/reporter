790c322805618f09342e521aa6239ab3993064f0
Karl Holme <karl.holme@evry.com>
2020-01-02 23:36:01 +0100

    Added styling

--- client/src/addPage/component.js
+++ client/src/addPage/component.js
@@ -5,38 +5,65 @@ export default function () {
     function AddPageContent({ store, state }) {
         return (
             <div className="card-block">
-                <h1>FELANMÃ„LAN</h1>
+                <h1>FELANMÄLAN</h1>
 
                 <form>
-                    <label>
-                        Rubrik:
-                        <br />
-                        <input
-                            value={state.addReport.header}
-                            type="text"
-                            name="Rubrik"
-                            onChange={(event) => {
-                                store.dispatch({ type: 'UPDATE_FORM', data: event.target.value, inputField: 'header' })
-                            }}
-                        />
-                    </label>
+                    <div id="wrapper">
 
-                    <br />
-                    <label>
-                        <br />
-                        Beskrivning:
-                        <br />
+                        <div id="left">
+                            <fieldset>
+                                <label>Rubrik:</label>
+                                <input value={state.addReport.header}
+                                    type="text"
+                                    name="header"
+                                    style={{ width: '300px' }}
+                                    onChange={(event) => {
+                                        store.dispatch({ type: 'UPDATE_FORM', data: event.target.value, inputField: 'header' })
+                                    }} />
+                            </fieldset>
+                        </div>
+
+                        <div id="right">
+                            <fieldset className="mb-5">
+                                <label className="float-right">Fastighetsnummer:</label>
+                                <input
+                                    className="float-right"
+                                    value={state.addReport.propertyNumber}
+                                    style={{ width: '150px' }}
+                                    type="number"
+                                    name="propertynumber"
+                                    onChange={(event) => {
+                                        store.dispatch({ type: 'UPDATE_FORM', data: event.target.value, inputField: 'propertyNumber' })
+                                    }} />
+                            </fieldset>
+                        </div>
+                    </div>
+
+                    <fieldset>
+                        <label>Plats:</label>
+                        <input value={state.addReport.place}
+                            type="text"
+                            name="place"
+                            style={{ width: '300px' }}
+                            onChange={(event) => {
+                                store.dispatch({ type: 'UPDATE_FORM', data: event.target.value, inputField: 'place' })
+                            }} />
+                    </fieldset>
+
+                    <fieldset>
+                        <label>Beskrivning:</label>
                         <textarea
                             value={state.addReport.description}
                             type="text"
                             name="Beskrivning"
+                            maxLength="700"
+                            rows="4"
+                            placeholder="Beskriv felet här..."
                             onChange={(event) => {
                                 store.dispatch({ type: 'UPDATE_FORM', data: event.target.value, inputField: 'description' })
-                            }}
-                        />
-                    </label>
-                    <br />
-                    <input type="submit" value="Submit" />
+                            }} />
+                    </fieldset>
+                    <input className="btn-primary" type="submit" value="Skicka" />
                 </form>
             </div>
         )
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
-import "./styles.css";
+//import "./style/bootstrap.css";
+import "./style/styles.css";
 import store from './../stores/configureStore'
 
 
